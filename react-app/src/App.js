1db786b0a782748f864c078ce768e2f6b9289385
unknown <psaikumar4268@.com>
2020-05-02 20:35:55 +0530

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+      class Buttons extends React.Component {
+        constructor(){
+          super();
+          this.state = {val: 0}
+          this.update = this.update.bind(this)
+        }
+         update(){
+           this.setState({val: this.state.val +1)}
+
+        }
+      com
+      
+      )
+      }
+    }
+export default App
